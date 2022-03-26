97aeb04b24c0581fdb2fda4310ca25dd58cd1688
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-26 10:43:53 +0530

    Ability to Delete contacts in Address Book

--- src/main/java/com/day28/AddressBook.java
+++ src/main/java/com/day28/AddressBook.java
@@ -9,9 +9,6 @@ package com.day28;
  * UC3:- Ability to edit existing contact person using their name
  * 
  * UC4:- Ability to delete a person using person's name
- * 
- * UC5:- Ability to add multiple person to Address Book
- * 
  * @author user -Almas
  *
  */
@@ -21,115 +18,83 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-
+	static ContactDetails person = new ContactDetails();
 	/**
 	 * Creating a List of ContactDetails of type String using ArrayList here Adding
 	 * new elements in the List
 	 */
-	List<ContactDetails> contactDetailsList = new ArrayList<ContactDetails>();
-	Scanner scanner = new Scanner(System.in);
+	static List<ContactDetails> contactDetailsList = new ArrayList<ContactDetails>();
+	static Scanner scanner = new Scanner(System.in);
 
 	/**
-	 * Declaring The Add Contact Method And Entering The Contact Details By Using
-	 * Scanner Class And Printing The Contact Details Of Person
+	 * created method addNewContact() to create a new contacts to the AddressBook
 	 */
 	public void addContact() {
-		System.out.println("Enter the number of contacts you want to enter");
-		int number = scanner.nextInt();
-		for (int i = 0; i < number; i++) {
-			System.out.println("Enter the contact details of person ");
-			writeContact();
-		}
-	}
-
-	/**
-	 * created method writeContact() to create a new contacts to the AddressBook
-	 */
-	public void writeContact() {
-		ContactDetails person = new ContactDetails();
 		System.out.println("Enter First Name : ");
 		String firstName = scanner.next();
 		System.out.println("Enter Last Name : ");
 		String lastName = scanner.next();
-		System.out.println("Enter Address : ");
+		System.out.println("Enter Your Address : ");
 		String address = scanner.next();
-		System.out.println("Enter City : ");
+		System.out.println("Enter Your City : ");
 		String city = scanner.next();
-		System.out.println("Enter State : ");
+		System.out.println("Enter Your State");
 		String state = scanner.next();
 		System.out.println("Enter ZipCode : ");
 		int zipCode = scanner.nextInt();
-		System.out.println("Enter Mobile Number : ");
+		System.out.println("Enter MobileNumber : ");
 		long mobileNumber = scanner.nextLong();
-		System.out.println("Enter EmailId : ");
+		System.out.println("Enter your EmailId  : ");
 		String emailId = scanner.next();
+
 		person = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
 		contactDetailsList.add(person);
 		printContact();
+
 	}
 
 	/**
-	 * Declaring The Edit Contact Method TO Edit The Details Of Contact The Details
-	 * Of Contact Edit By Using FirstName If First Name Is Match The Contact Will
-	 * Edit
+	 * created method editContact() to editing the contacts
 	 */
 	public void editContact() {
-		System.out.println("Enter the first name of person to edit contact");
-		String editName = scanner.next();
-		boolean edited = false;
-		for (int i = 0; i < contactDetailsList.size(); i++) {
-			String name = contactDetailsList.get(i).getFirstName();
-			if (name.equalsIgnoreCase(editName)) {
-				writeContact();
-				edited = true;
-				break;
-			}
-		}
-		if (!edited) {
-			System.out.println("enter name is incorrect");
+		System.out.println("Enter the first name of person to edit Contact : ");
+		String firstName = scanner.next();
+		if (firstName.equalsIgnoreCase(person.getFirstName())) {
+			addContact();
+		} else {
+			System.out.println("The Entered First Name Is Not Match");
+			editContact();
 		}
 	}
 
 	/**
-	 * Declaring Delete Contact Method TO delete The Details Of Contact The Details
-	 * Of Contact Delete By Using FirstName If First Name Is Match Then Contact Will
-	 * Delete
+	 * created method deleteContact() to deleting the contacts
 	 */
 	public void deleteContact() {
-		System.out.println("Enter the first name of person to delete contact");
+		System.out.println("Enter the First name of Person to Delete Contact : ");
 		String deleteName = scanner.next();
-		int i = 0;
-		for (; i < contactDetailsList.size(); i++) {
-			String name = contactDetailsList.get(i).getFirstName();
-			if (name.equalsIgnoreCase(deleteName)) {
-				break;
-			}
-		}
-		if (i < contactDetailsList.size()) {
-			contactDetailsList.remove(i);
-			System.out.println("Contact Deleted");
-			System.out.println("Remaining contacts in the book isgit");
-			printContact();
+		if (deleteName.equalsIgnoreCase(person.getFirstName())) {
+			System.out.println("Deleted " + person.getFirstName() + " details");
+			person = null;
 		} else {
-			System.out.println("Contact not find");
+			System.out.println("The Entered First Name is Not Matched");
+			deleteContact();
 		}
-
 	}
 
 	/**
 	 * created method printContact() to display all the contacts
 	 */
-	public void printContact() {
+	public static void printContact() {
 		for (int i = 0; i < contactDetailsList.size(); i++) {
+			person = contactDetailsList.get(i);
 			System.out.println("Contact Details");
-			System.out.println("Name         : " + contactDetailsList.get(i).getFirstName() + " "
-					+ contactDetailsList.get(i).getLastName() + "\n" + "Address      : "
-					+ contactDetailsList.get(i).getAddress() + "\n" + "City         : "
-					+ contactDetailsList.get(i).getCity() + "\n" + "State        : "
-					+ contactDetailsList.get(i).getState() + "\n" + "ZipCode      : "
-					+ contactDetailsList.get(i).getZipCode() + "\n" + "MobileNumber : "
-					+ contactDetailsList.get(i).getMobileNumber() + "\n" + "EmailId      : "
-					+ contactDetailsList.get(i).getEmailId() + "\n");
+			System.out.println("Name          : " + person.getFirstName() + " " + person.getLastName() + "\n"
+					+ "Address       : " + person.getAddress() + "\n" + "City          : " + person.getCity() + "\n"
+					+ "State         : " + person.getState() + "\n" + "ZipCode       : " + person.getZipCode() + "\n"
+					+ "MobileNumber  : " + person.getMobileNumber() + "\n" + "EmailId       : " + person.getEmailId()
+					+ "\n");
 		}
 	}
+
 }
